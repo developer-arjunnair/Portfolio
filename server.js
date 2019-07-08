488d6c5984ef669a0d12bc9439ca167cbd4778ea
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2019-07-07 21:54:38 -0400

    updating build file location

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
