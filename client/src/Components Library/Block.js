1a5db43ba3340d825af055f01566532962554d77
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2020-10-26 01:35:53 -0400

    Updates

--- client/src/Components Library/Block.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GridLayout = () => {
-  return <div />;
-};
-
-export default GridLayout;
---
+++ precache-manifest.2b60f1f6e95bebfd6552bbc33671fdea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "874cd7e8eabffa19bd52",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/css/main.25a7ff8e.chunk.css"
+  },
+  {
+    "revision": "874cd7e8eabffa19bd52",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/js/main.874cd7e8.chunk.js"
+  },
+  {
+    "revision": "2866718d0f930cfac41e",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/js/runtime~main.2866718d.js"
+  },
+  {
+    "revision": "28f4135b4ef7d6f31bb8",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/css/2.b8c9c85f.chunk.css"
+  },
+  {
+    "revision": "28f4135b4ef7d6f31bb8",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/js/2.28f4135b.chunk.js"
+  },
+  {
+    "revision": "9e3bcd35fc9d5758e786f814ab7636a0",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/WiproRoundLogo.9e3bcd35.png"
+  },
+  {
+    "revision": "70e461567826b222ca37dfa4df839783",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/ProPic.70e46156.jpg"
+  },
+  {
+    "revision": "4fecab77089b0ccdcd56032fff958fdc",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/Expedia round.4fecab77.png"
+  },
+  {
+    "revision": "7ec46c45c220282fa70193c4f4fe5a3e",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/wayfair-logo.7ec46c45.png"
+  },
+  {
+    "revision": "6c77dc0fe9bc266a9c0bc9c11dabc2b4",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/Expedia.6c77dc0f.png"
+  },
+  {
+    "revision": "b0007b71a0d9a215b632d94d5c2c61df",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/index.html"
+  }
+];
