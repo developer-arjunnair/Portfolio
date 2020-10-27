5d3b3af68186f95ad546f263b9c1095494e5108b
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2020-10-27 08:31:32 -0400

    Updates

--- client/src/SharedComponents/BSContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const BSContainer = ({ children, className, ...otherProps }) => (
-  <div className={`container-fluid ${className}`} {...otherProps}>
-    {children}
-  </div>
-);
-export default BSContainer;
---
+++ precache-manifest.f1324b166b7ba64deb666a01f53ef861.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6970c5e69f4c6d58b104",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/css/main.6dbd592e.chunk.css"
+  },
+  {
+    "revision": "6970c5e69f4c6d58b104",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/js/main.6970c5e6.chunk.js"
+  },
+  {
+    "revision": "2866718d0f930cfac41e",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/js/runtime~main.2866718d.js"
+  },
+  {
+    "revision": "abb3a4b04ea0864866e8",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/css/2.b8247490.chunk.css"
+  },
+  {
+    "revision": "abb3a4b04ea0864866e8",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/js/2.abb3a4b0.chunk.js"
+  },
+  {
+    "revision": "9e3bcd35fc9d5758e786f814ab7636a0",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/WiproRoundLogo.9e3bcd35.png"
+  },
+  {
+    "revision": "70e461567826b222ca37dfa4df839783",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/ProPic.70e46156.jpg"
+  },
+  {
+    "revision": "4fecab77089b0ccdcd56032fff958fdc",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/Expedia round.4fecab77.png"
+  },
+  {
+    "revision": "7ec46c45c220282fa70193c4f4fe5a3e",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/wayfair-logo.7ec46c45.png"
+  },
+  {
+    "revision": "6c77dc0fe9bc266a9c0bc9c11dabc2b4",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/static/media/Expedia.6c77dc0f.png"
+  },
+  {
+    "revision": "f5a047e9578484139c951f52cfcb42fd",
+    "url": "/developer-arjunnair/developer-arjunnair.github.io/index.html"
+  }
+];
