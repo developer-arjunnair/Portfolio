d4b902bdc49790bf2299698ee34c3bab7eff8e29
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2020-11-08 09:31:13 -0500

    smtp changes

---
+++ client/src/utils/smtp.js
@@ -0,0 +1,38 @@
+﻿/* SmtpJS.com - v3.0.0 */
+const Email = {
+  send: function (a) {
+    return new Promise(function (n, e) {
+      a.nocache = Math.floor(1e6 * Math.random() + 1);
+      a.Action = "Send";
+      var t = JSON.stringify(a);
+      console.log("the data -->>>> ", a);
+      Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+        n(e);
+      });
+    });
+  },
+  ajaxPost: function (e, n, t) {
+    var a = Email.createCORSRequest("POST", e);
+    a.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    a.onload = function () {
+      var e = a.responseText;
+      null != t && t(e);
+    };
+    a.send(n);
+  },
+  ajax: function (e, n) {
+    var t = Email.createCORSRequest("GET", e);
+    t.onload = function () {
+      var e = t.responseText;
+      null != n && n(e);
+    };
+    t.send();
+  },
+  createCORSRequest: function (e, n) {
+    var t = new XMLHttpRequest();
+    t.open(e, n);
+    return t;
+  },
+};
+
+export default Email;
